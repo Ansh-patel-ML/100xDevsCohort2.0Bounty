871c1942455eca7c63eaada8106295a0fcff46c0
Ansh Patel <patelansh883@gmail.com>
2023-12-08 19:09:54 +0530

    Added segments

--- index.js
+++ index.js
@@ -1,191 +1,177 @@
-class VideoSegmentManager {
-  constructor(videoId) {
-    // Initialize Video.js player with specified options
-    this.player = videojs(
-      videoId,
-      {
-        controls: true,
-        fluid: true,
-        html5: {
-          vhs: {
-            overrideNative: true,
-          },
-        },
+var player = videojs(
+  "my-video",
+  {
+    controls: true,
+    fluid: true,
+    html5: {
+      vhs: {
+        overrideNative: true,
       },
-      this.initialize.bind(this) // Binding the initialize method to ensure the correct 'this' context
-    );
-
-    // Object to store video segments
-    this.segmentObj = {};
-  }
-
-  initialize() {
-    // Initialize Encrypted Media Extensions (EME) for DRM
-    this.player.eme();
-
-    // Set video source and DRM configuration
-    this.player.src({
+    },
+  },
+  function () {
+    var player = this;
+    player.eme();
+    player.src({
       src: "https://cdn.bitmovin.com/content/assets/art-of-motion_drm/mpds/11331.mpd",
       type: "application/dash+xml",
       keySystems: {
         "com.widevine.alpha": "https://cwip-shaka-proxy.appspot.com/no_auth",
       },
     });
-
-    // Start playing the video
-    const x = this.player.play();
-
-    // Setup event listeners for adding segments
-    this.setupEventListeners();
-
-    // Start interval to update segment styles periodically
-    this.startUpdateInterval();
-
-    // Error container element
-    this.errorContainer = document.getElementById("error-container");
-  }
-
-  setupEventListeners() {
-    // Add event listener to the "Add Segment" button
-    document.getElementById("add-segment").addEventListener("click", () => {
-      // Get start and end times from input fields and add a segment
-      const startTime = parseInt(document.getElementById("start-time").value);
-      const endTime = parseInt(document.getElementById("end-time").value);
-      const segmentTitle = document.getElementById("segment-title").value;
-      this.addSegment(startTime, endTime, segmentTitle);
-    });
-  }
-
-  startUpdateInterval() {
-    // Set interval to update segment styles every 500 milliseconds
-    setInterval(this.updateSegmentStyles.bind(this), 500);
-  }
-
-  // Method to add a new segment to the video
-  addSegment(start, end, segmentTitle) {
-    // Generate a new segment ID
-    const newSegmentId = Object.keys(this.segmentObj).length + 1;
-
-    if (!this.isValidTime(start) || !this.isValidTime(end)) {
-      this.showError(
-        "Please enter valid positive whole numbers for start and end times."
-      );
-      return; // Exit method if there's an error
-    }
-
-    // Validate segment timing
-    if (newSegmentId === 1 && start !== 0) {
-      throw new Error("The first segment must start from 0 seconds.");
-    }
-
-    const lastSegment = this.segmentObj[Object.keys(this.segmentObj).length];
-
-    if (lastSegment && start !== lastSegment.end) {
-      throw new Error("Segments must be continuous. Please adjust start time.");
-    }
-
-    if (newSegmentId !== 1 && start < lastSegment.end) {
-      throw new Error("Segments must be continuous. Please adjust start time.");
-    }
-
-    // Add segment to the segment object
-    this.segmentObj[newSegmentId] = {
-      start: start,
-      end: end,
-      segmentTitle: `${segmentTitle}`,
+    const x = player.play();
+    const segmentObj = {
+      1: {
+        start: 0,
+        end: 30,
+        segmentTitle: "Segment 1",
+      },
+      2: {
+        start: 30,
+        end: 60,
+        segmentTitle: "Segment 2",
+      },
+      3: {
+        start: 60,
+        end: 90,
+        segmentTitle: "Segment 3",
+      },
+      4: {
+        start: 90,
+        end: 120,
+        segmentTitle: "Segment 4",
+      },
+      5: {
+        start: 120,
+        end: 150,
+        segmentTitle: "Segment 5",
+      },
     };
 
-    // Update segment display on the video progress bar
-    this.updateSegmentDisplay(newSegmentId, start, end, segmentTitle);
-  }
+    function handleWidthChange(entries) {
+      for (let entry of entries) {
+        // Do something when the width changes
+        // Call your custom function here
+        yourCustomFunction(entry.contentRect.width);
+      }
+    }
+    const customProgressDot = document.createElement("div");
+    customProgressDot.className = "custom-progress-dot";
 
-  isValidTime(time) {
-    return Number.isInteger(time) && time >= 0;
-  }
+    const targetElement =
+      document.getElementsByClassName("vjs-play-progress ")[0];
 
-  showError(errorMessage) {
-    // Display error message in the error container
-    this.errorContainer.textContent = errorMessage;
-  }
+    const y = document.getElementsByClassName("vjs-progress-holder")[0];
 
-  // Method to update segment styles based on current video time
-  updateSegmentStyles() {
-    const currentTime = this.player.currentTime();
+    y.appendChild(customProgressDot);
 
-    for (let i = 1; i <= Object.keys(this.segmentObj).length; i++) {
-      const segment = document.querySelector(`.segment-${i}`);
+    const resizeObserver = new ResizeObserver(handleWidthChange);
 
-      if (currentTime >= this.segmentObj[i].end) {
-        // Style completed segments
+    resizeObserver.observe(targetElement);
+
+    function yourCustomFunction(newWidth) {
+      customProgressDot.style.width = newWidth + "px";
+    }
+
+    setTimeout(() => {
+      const vedioProgressBar = document.getElementsByClassName(
+        "vjs-progress-holder"
+      )[0];
+      const vedioPlayProgress =
+        document.getElementsByClassName("vjs-play-progress")[0];
+
+      const vedioToolTipContainer =
+        document.getElementsByClassName("vjs-mouse-display")[0];
+
+      const vedioToolTipTimeStamp =
+        document.getElementsByClassName("vjs-time-tooltip")[0];
+
+      const vedioProgressBarWidth = vedioProgressBar.clientWidth;
+      const totalDuration = player.duration();
+      const pixelPerSecond = vedioProgressBarWidth / totalDuration;
+
+      vedioPlayProgress.style.zIndex = "30";
+      vedioProgressBar.style.display = "flex";
+      vedioPlayProgress.classList.add("disable-hover");
+      for (let i = 1; i <= Object.keys(segmentObj).length; i++) {
+        const segment = document.createElement("div");
+
+        segment.className = `segment-${i} scale`;
+        segment.style.border = "0px";
+        segment.style.borderRight = "3px solid rgba(43,51,63,.7)";
+        segment.style.height = "0.3em";
         segment.style.zIndex = "50";
-        segment.style.borderRight = "5px solid rgba(43,51,63,1)";
+        segment.style.width =
+          (segmentObj[i].end - segmentObj[i].start) * pixelPerSecond + "px";
+
+        vedioToolTipContainer.style.display = "flex";
+        vedioToolTipContainer.style.flexDirection = "column-reverse";
+        vedioToolTipContainer.style.gap = "5px";
+        vedioToolTipContainer.style.alignItems = "center";
+        vedioToolTipTimeStamp.style.position = "unset";
+        vedioToolTipContainer.style.top = "-20px";
+
+        segment.addEventListener("mouseover", () => {
+          if (segmentObj[i].end >= player.currentTime()) {
+            segment.style.cursor = "pointer";
+            segment.style.backgroundColor = "rgba(115,133,159, 1)";
+            segment.style.zIndex = "15";
+            segment.style.height = "0.35em";
+            customProgressDot.style.height = "0.35em";
+          } else {
+            segment.style.borderRight = "3px solid rgba(43,51,63,1)";
+            segment.style.backgroundColor = "white";
+            segment.style.height = "0.35em";
+          }
+          handleSegmentTitle(
+            segmentObj[i].segmentTitle,
+            vedioToolTipContainer,
+            i
+          );
+        });
+        segment.addEventListener("mouseout", () => {
+          if (segmentObj[i].end >= player.currentTime()) {
+            segment.style.backgroundColor = "transparent";
+            segment.style.scale = "1";
+            segment.style.zIndex = "50";
+            segment.style.height = "0.3em";
+            customProgressDot.style.height = "0.3em";
+          } else {
+            segment.style.height = "0.3em";
+            segment.style.backgroundColor = "transparent";
+          }
+
+          handleSegmentTitle(
+            segmentObj[i].segmentTitle,
+            vedioToolTipContainer,
+            i,
+            "REMOVE"
+          );
+        });
+        vedioProgressBar.appendChild(segment);
+      }
+    }, 5000);
+
+    function handleSegmentTitle(
+      segmentTitle,
+      toolTipContainer,
+      segmentNumber,
+      flag = "ADD"
+    ) {
+      if (flag === "ADD") {
+        const segmentTitleContainer = document.createElement("div");
+        segmentTitleContainer.className = `vjs-time-tooltip segment-title-${segmentNumber}`;
+        segmentTitleContainer.style.position = "unset";
+        segmentTitleContainer.style.width = "100px";
+        segmentTitleContainer.style.backgroundColor = "rgba(43,51,63,1)";
+        segmentTitleContainer.textContent = segmentTitle;
+        toolTipContainer.appendChild(segmentTitleContainer);
       } else {
-        // Style ongoing segments
-        segment.style.zIndex = "15";
-        segment.style.borderRight = "5px solid rgba(43,51,63,.7)";
+        const segmentTitle = document.getElementsByClassName(
+          `segment-title-${segmentNumber}`
+        )[0];
+        toolTipContainer.removeChild(segmentTitle);
       }
     }
   }
-
-  // Method to update the visual representation of segments on the progress bar
-  updateSegmentDisplay(segmentId, start, end, segmentTitle) {
-    // Calculate dimensions for the segment representation
-    const vedioToolTip =
-      document.getElementsByClassName("vjs-mouse-display")[0];
-    const vedioPlayProgress =
-      document.getElementsByClassName("vjs-play-progress")[0];
-    const vedioToolTipTimeStamp =
-      document.getElementsByClassName("vjs-time-tooltip")[0];
-    const vedioProgressBar = document.getElementsByClassName(
-      "vjs-progress-holder"
-    )[0];
-    const vedioProgressBarWidth = vedioProgressBar.clientWidth;
-    const pixelPerSecond = vedioProgressBarWidth / this.player.duration();
-    vedioPlayProgress.style.zIndex = "30";
-    vedioProgressBar.style.display = "flex";
-
-    // Create a div element to represent the segment visually
-    const segment = document.createElement("div");
-    segment.className = `segment-${segmentId} scale`;
-    segment.style.border = "0px";
-    segment.style.borderRight = "3px solid rgba(43,51,63,.7)";
-    segment.style.height = "0.3em";
-    segment.style.zIndex = "15";
-    segment.style.width = (end - start) * pixelPerSecond + "px";
-
-    // Add a text label to the segment representation
-
-    const segmentLabel = document.createElement("div");
-    segmentLabel.className = "vjs-time-tooltip";
-    segmentLabel.textContent = `${segmentTitle}`;
-    vedioToolTip.appendChild(segmentLabel);
-    vedioToolTip.style.display = "flex";
-    vedioToolTip.style.flexDirection = "column-reverse";
-    vedioToolTip.style.gap = "5px";
-    vedioToolTip.style.alignItems = "center";
-    segmentLabel.style.position = "unset";
-    segmentLabel.style.width = "fit-content";
-    vedioToolTipTimeStamp.style.position = "unset";
-    vedioToolTip.style.top = "-20px";
-
-    // Add mouseover and mouseout event listeners for segment interaction
-    segment.addEventListener("mouseover", () => {
-      segment.style.cursor = "pointer";
-      segment.style.backgroundColor = "rgba(115,133,159, 1)";
-      segment.style.zIndex = "0";
-      segmentLabel.style.display = "contents";
-    });
-    segment.addEventListener("mouseout", () => {
-      segment.style.backgroundColor = "transparent";
-      segment.style.scale = "1";
-      segment.style.zIndex = "15";
-      // segmentLabel.style.display = "none";
-    });
-
-    // Append the segment representation to the video progress bar
-    vedioProgressBar.appendChild(segment);
-  }
-}
-
-// Create an instance of VideoSegmentManager
-const segmentManager = new VideoSegmentManager("my-video");
+);
